975391711a7fc513b2fef9f4aa18cbb05a802089
Pavel <nuar89@mail.ru>
2023-08-04 17:50:54 +0300

    Initial Commit

---
+++ TodOLisT/TodOLisTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodOLisTApp.swift
+//  TodOLisT
+//
+//  Created by Павел Галкин on 04.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodOLisTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
